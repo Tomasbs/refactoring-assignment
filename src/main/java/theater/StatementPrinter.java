af45d56e668a44234e14c5cf6abf4f9f6b11afb5
Tomas Souto <tomasbsouto@gmail.com>
2025-11-23 16:51:18 -0500

    Task 2.3

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -27,22 +27,34 @@ public class StatementPrinter {
         final StringBuilder result = new StringBuilder("Statement for " + invoice.getCustomer()
                 + System.lineSeparator());
 
-        final NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-
         for (Performance p : invoice.getPerformances()) {
 
             // add volume credits
             volumeCredits += getVolumeCredits(p);
 
-            // print line for this order
-            result.append(String.format("  %s: %s (%s seats)%n", getPlay(p).getName(),
-                    frmt.format(getThisAmount(p) / Constants.PERCENT_FACTOR), p.getAudience()));
+            // print line for this order (UPDATED to use usd())
+            result.append(String.format(
+                    "  %s: %s (%s seats)%n",
+                    getPlay(p).getName(),
+                    usd(getThisAmount(p)),
+                    p.getAudience()
+            ));
+
             totalAmount += getThisAmount(p);
         }
-        result.append(String.format("Amount owed is %s%n", frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
+
+        // UPDATED to use usd()
+        result.append(String.format("Amount owed is %s%n", usd(totalAmount)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
         return result.toString();
     }
+
+    private String usd(int amount) {
+        return NumberFormat.getCurrencyInstance(Locale.US)
+                .format(amount / Constants.PERCENT_FACTOR);
+    }
+
+
 
     private int getVolumeCredits(Performance performance) {
         int result = 0;
